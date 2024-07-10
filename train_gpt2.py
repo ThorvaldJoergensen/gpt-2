51c0bddc41686d3b34edcef1c27646dad468676e
Thorvald Jørgensen <thorjor@dfds.com>
2024-07-10 13:41:53 +0200

    Added checkpoints every 5000th step

--- train_gpt2.py
+++ train_gpt2.py
@@ -427,6 +427,7 @@ torch.set_float32_matmul_precision("high")
 
 # Create model
 model = GPT(GPTConfig(vocab_size=50304))  # Nearest power of 2 to 50257
+# model = GPT.from_pretrained("gpt2") # or init from OpenAI GPT-2
 model.to(device)
 use_compile = (
     False  # torch.compile interferes with HellaSwag eval and Generation. TODO fix
@@ -442,7 +443,9 @@ raw_model = model.module if ddp else model  # always contains the "raw" unwrappe
 max_lr = 6e-4
 min_lr = max_lr * 0.1
 warmup_steps = 715
-max_steps = 19073
+max_steps = (
+    19073  # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+)
 
 
 def get_lr(it):
@@ -499,6 +502,18 @@ for step in range(max_steps):
             print(f"validation loss: {val_loss_accum.item():.4f}")
             with open(log_file, "a") as f:
                 f.write(f"{step} val {val_loss_accum.item():.4f}\n")
+            if step > 0 and (step % 5000 == 0 or last_step):
+                # Optionally save a checkpoint
+                checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+                checkpoint = {
+                    "model": raw_model.state_dict(),
+                    "config": raw_model.config,
+                    "step": step,
+                    "val_loss": val_loss_accum.item(),
+                }
+                # You might also want to add optimizer.state_dict() here
+                # and rng seeds etc., if you wanted to more exactly resume training
+                torch.save(checkpoint, checkpoint_path)
 
     # once in a while evaluate hellaswag
     if (step % 250 == 0 or last_step) and (not use_compile):
